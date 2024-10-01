36a86097b9ca5217ca24d72a073e71e5dc1fb450
rhmti01 <rahmati.business@gmail.com>
2024-10-01 21:43:08 +0330

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html", " ./src/js/app.js "],
+  // darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
