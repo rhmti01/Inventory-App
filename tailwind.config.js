dd0f5085b72f50ca05d68a71c251c8bc89a4ba5d
rhmti01 <rahmati.business@gmail.com>
2024-10-04 17:16:38 +0330

    Full Ui Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,33 @@ module.exports = {
   content: ["./public/**/*.html", " ./src/js/app.js "],
   // darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'zz':'700px',
+        'ww':'560px',
+        'xx':'480px',
+        'dd':'365px',
+        'ss':'285px',
+      },
+      colors: {
+        // 'main': '#A8763E',
+        // 'main':'#fb6f92',
+        // 'main':'#0b090a',
+        // 'main':'#ba181b',
+        // 'main':'#55a630',
+        // 'main':'#4361ee',
+        // 'main':'#e7bc91',
+        // 'main':'#588157',
+        // 'main':'#6c757d',
+        // 'main':'#b9375e',
+        // 'main':'#ffa200',
+        'main':'#141E22',
+      },
+      fontFamily: {
+        outfit: ['Outfit'],
+        vazir: ['Vazirmatn']
+      },
+    },
   },
   plugins: [],
 }
