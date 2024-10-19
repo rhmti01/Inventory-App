b9d3da159030cf0d7b13a7d3585d8eebe07394be
rhmti01 <rahmati.business@gmail.com>
2024-10-20 00:27:08 +0330

    merge babel built files into webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require("path")
 module.exports = {
     entry: [
-        "./src/js/app.js",
-        "./src/js/storage.js",
-        "./src/js/categoryView.js",
-        "./src/js/productView.js",
+        "./public/build/babel/app.js",
+        "./public/build/babel/storage.js",
+        "./public/build/babel/categoryView.js",
+        "./public/build/babel/productView.js",
     ],
     output : {
         path : path.resolve(__dirname, "public/build/webpack")  ,
